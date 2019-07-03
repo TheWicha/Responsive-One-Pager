a53afba90d0434f1e468fb8077956e9599c7e749
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-07-03 23:18:03 +0200

    Add Header component with styles

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './style.scss'
+import img from '../../img/ipad.png'
+
+function About(props) {
+  return (
+    <section className='about'>
+      <div className='about-container'>
+        <div className='about-center'>
+          <h2>Build with passion!</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aliquam eget augue id orci vestibulum varius.
+            Curabitur orci mi, rutrum sed tincidunt vel, dictum efficitur leo.
+            Nullam cursus, eros tincidunt pulvinar rutrum.
+          </p>
+        </div>
+        <img className='about-img' alt='' src={img} />
+      </div>
+    </section>
+  )
+};
+
+
+export default About;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style.scss'
+
+function Header(props) {
+    return (
+        <header className='header'>
+          <div className='header-container'>
+            <div className='header-center'>
+              <h1>Port Project</h1>
+              <h2>A cool, responsive one page created by Daniel Wisniewski</h2>
+              <a href="#about">Let's Go!</a>
+            </div>
+          </div>
+        </header>
+    )
+  }
+
+
+
+export default Header;
