edc337d568129ef6095277db1ceefbe912458281
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-07-12 14:35:46 +0200

    Add font awsome package with social media to project

--- src/components/Contact/Socials/icons.js
+++ src/components/Contact/Socials/icons.js
@@ -1,14 +1,18 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faFacebookF, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 export default [
   {
-    name: "fab fa-twitter",
+    name: <FontAwesomeIcon icon={faTwitter} />,
     href: "https://twitter.com"
   },
   {
-    name: "fab fa-facebook-f",
+    name: <FontAwesomeIcon icon={faFacebookF} />,
     href: "https://facebook.com"
   },
   {
-    name: "fab fa-github",
+    name: <FontAwesomeIcon icon={faGithub} />,
     href: "https://github.com/TheWicha/Responsive-One-Pager"
   },
 ]
--- src/components/Contact/Socials/index.js
+++ src/components/Contact/Socials/index.js
@@ -8,7 +8,7 @@ function Socials(props) {
 <div className="socials">
   {icons.map((icon, i) => (
     <a key={i} href={icon.href} target="_blank" rel="noopener noreferrer">
-      <i className={icon.name}></i>
+      {icon.name}
     </a>
   ))}
 </div>
